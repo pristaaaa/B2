9caed89afbe7d4e206ad4c4a30e5bcf6b81d3c2c
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-03 21:34:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getMPDFile() {
+  return "https://linearjitp-playback.astro.com.my/hls-mp4-fp/linear/5066/default.m3u8";
+}
+
+function getClearKey() {
+  return "5102b12aac7756c65dcb46a101d960d3";
+}
+
+function getClearKeyId() {
+  return "39c175581e237eff9559607eb9b23c10";
+}
